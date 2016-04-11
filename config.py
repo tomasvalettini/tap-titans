b795f3ec820a50e59d6f9a1eb0746b9a785dede4
tomas <tomasvalettini@gmail.com>
2016-04-11 14:20:24 -0400

    db for heroku

--- config.py
+++ config.py
@@ -4,5 +4,12 @@ WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Gameboy101!@localhost/tap-titans'
+if os.environ.get('DATABASE_URL') is None:
+    SQLALCHEMY_DATABASE_URI = ('sqlite:///' + os.path.join(basedir, 'app.db') +
+                               '?check_same_thread=False')
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+# SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Gameboy101!@localhost/tap-titans'
+# SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
